1ff54e74918c2bca61eb73ddb2927f780cd7b843
Joey Sacchini <joey@sacchini.net>
2025-05-23 13:19:46 -0400

    add cfb8 encryption support
    Signed-off-by: Joey Sacchini <joey@sacchini.net>

--- src/CraftConn.zig
+++ src/CraftConn.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const net = std.net;
 const craft_io = @import("io.zig");
+const cfb8 = @import("cfb8.zig");
 
 const BUF_SIZE = 256;
 const bufReader = std.io.BufferedReader(BUF_SIZE, net.Stream.Reader);
@@ -12,19 +13,20 @@ address: []const u8,
 port: u16,
 reader: bufReader,
 buf: std.ArrayList(u8),
-compression: Compression = .disabled,
+compression: ?Compression = null,
+encryption: ?Encryption = null,
 allocator: std.mem.Allocator,
 
 const Conn = @This();
 
-const Compression = union(enum) {
-    enabled: Enabled,
-    disabled,
+const Compression = struct {
+    threshold: usize,
+    buf: std.ArrayList(u8),
+};
 
-    const Enabled = struct {
-        threshold: usize,
-        buf: std.ArrayList(u8),
-    };
+const Encryption = struct {
+    rx: cfb8.Cipher,
+    tx: cfb8.Cipher,
 };
 
 pub fn connect(allocator: std.mem.Allocator, address: []const u8, port: u16) !Conn {
@@ -42,10 +44,10 @@ pub fn connect(allocator: std.mem.Allocator, address: []const u8, port: u16) !Co
 }
 
 pub fn deinit(conn: *Conn) void {
-    switch (conn.compression) {
-        .enabled => |c| c.buf.deinit(),
-        else => {},
+    if (conn.compression) |*compression| {
+        compression.buf.deinit();
     }
+
     conn.buf.deinit();
     conn.socket.close();
     log.debug("{*} connection shutdown", .{conn});
@@ -106,50 +108,55 @@ pub fn readAndExpectPacket(conn: *Conn, expected_id: i32, comptime Packet: type,
 }
 
 pub fn readPacket(conn: *Conn) !ReadPkt {
+    const conn_reader = conn.reader.reader();
+    if (conn.encryption) |*encryption| {
+        return conn.readPacketFrom(encryption.rx.reader(conn_reader));
+    } else {
+        return conn.readPacketFrom(conn_reader);
+    }
+}
+
+fn readPacketFrom(conn: *Conn, reader: anytype) !ReadPkt {
     conn.clearBuffers();
 
-    const reader = conn.reader.reader();
     const pkt_length_decoded = try craft_io.decodeInt(i32, reader, .varnum);
     var bytes_read = pkt_length_decoded.bytes_read;
     var bytes_decoded = bytes_read;
     const pkt_length: usize = @intCast(pkt_length_decoded.value);
 
     var id_and_payload: []u8 = undefined;
-    switch (conn.compression) {
-        .enabled => |*compress| {
-            const data_length_decoded = try craft_io.decodeInt(i32, reader, .varnum);
-            bytes_read += data_length_decoded.bytes_read;
-            bytes_decoded += data_length_decoded.bytes_read;
-            const data_length: usize = @intCast(data_length_decoded.value);
-            const bytes_left = pkt_length - data_length_decoded.bytes_read;
-            if (data_length == 0) {
-                // packet is not compressed, just read it into the buffer
-                id_and_payload = try conn.buf.addManyAsSlice(bytes_left);
-                try reader.readNoEof(id_and_payload);
-                bytes_read += bytes_left;
-                bytes_decoded += bytes_left;
-            } else {
-                // packet is compressed, read it into the compression buffer
-                const compressed_buf = try compress.buf.addManyAsSlice(bytes_left);
-                try reader.readNoEof(compressed_buf);
-                bytes_read += bytes_left;
-
-                // decompress
-                var compressed_stream = std.io.fixedBufferStream(compressed_buf);
-                try std.compress.zlib.decompress(compressed_stream.reader(), conn.buf.writer());
-                bytes_decoded += conn.buf.items.len;
-                id_and_payload = conn.buf.items;
-                if (id_and_payload.len != data_length) {
-                    return error.DecompressDataLengthMismatch;
-                }
-            }
-        },
-        .disabled => {
-            id_and_payload = try conn.buf.addManyAsSlice(pkt_length);
+    if (conn.compression) |*compress| {
+        const data_length_decoded = try craft_io.decodeInt(i32, reader, .varnum);
+        bytes_read += data_length_decoded.bytes_read;
+        bytes_decoded += data_length_decoded.bytes_read;
+        const data_length: usize = @intCast(data_length_decoded.value);
+        const bytes_left = pkt_length - data_length_decoded.bytes_read;
+        if (data_length == 0) {
+            // packet is not compressed, just read it into the buffer
+            id_and_payload = try conn.buf.addManyAsSlice(bytes_left);
             try reader.readNoEof(id_and_payload);
-            bytes_read += pkt_length;
-            bytes_decoded += pkt_length;
-        },
+            bytes_read += bytes_left;
+            bytes_decoded += bytes_left;
+        } else {
+            // packet is compressed, read it into the compression buffer
+            const compressed_buf = try compress.buf.addManyAsSlice(bytes_left);
+            try reader.readNoEof(compressed_buf);
+            bytes_read += bytes_left;
+
+            // decompress
+            var compressed_stream = std.io.fixedBufferStream(compressed_buf);
+            try std.compress.zlib.decompress(compressed_stream.reader(), conn.buf.writer());
+            bytes_decoded += conn.buf.items.len;
+            id_and_payload = conn.buf.items;
+            if (id_and_payload.len != data_length) {
+                return error.DecompressDataLengthMismatch;
+            }
+        }
+    } else {
+        id_and_payload = try conn.buf.addManyAsSlice(pkt_length);
+        try reader.readNoEof(id_and_payload);
+        bytes_read += pkt_length;
+        bytes_decoded += pkt_length;
     }
 
     var id_payload_stream = std.io.fixedBufferStream(id_and_payload);
@@ -195,43 +202,44 @@ pub fn writePacketWithEncoding(
     const id_length = try craft_io.lengthInt(id, .varnum);
     const payload_and_id_length = payload_length + id_length;
     var bytes_encoded: usize = 0;
-    switch (conn.compression) {
-        .enabled => |*comp| {
-            // [length]
-            // [data length] (or 0)
-            // [compressed packet] (or uncompressed)
-            if (payload_and_id_length > comp.threshold) {
-                const data_length: i32 = @intCast(payload_and_id_length);
-                // write the compressed data to comp.buf
-                var compressor = try std.compress.zlib.compressor(comp.buf.writer(), .{});
-                const compressor_writer = compressor.writer();
-                bytes_encoded += try craft_io.encode(id, compressor_writer, allocator, .varnum);
-                bytes_encoded += try craft_io.encode(packet, compressor_writer, allocator, encoding);
-                try compressor.finish();
+    if (conn.compression) |*comp| {
+        // [length]
+        // [data length] (or 0)
+        // [compressed packet] (or uncompressed)
+        if (payload_and_id_length > comp.threshold) {
+            const data_length: i32 = @intCast(payload_and_id_length);
+            // write the compressed data to comp.buf
+            var compressor = try std.compress.zlib.compressor(comp.buf.writer(), .{});
+            const compressor_writer = compressor.writer();
+            bytes_encoded += try craft_io.encode(id, compressor_writer, allocator, .varnum);
+            bytes_encoded += try craft_io.encode(packet, compressor_writer, allocator, encoding);
+            try compressor.finish();
 
-                // write the packet we actually intend to send
-                const actual_size: usize = (try craft_io.length(data_length, allocator, .varnum)) + comp.buf.items.len;
-                const conn_buf_writer = conn.buf.writer();
-                bytes_encoded += try craft_io.encode(@as(i32, @intCast(actual_size)), conn_buf_writer, allocator, .varnum);
-                bytes_encoded += try craft_io.encode(data_length, conn_buf_writer, allocator, .varnum);
-                try conn.buf.appendSlice(comp.buf.items);
-            } else {
-                _ = try craft_io.encode(@as(i32, @intCast(payload_and_id_length + 1)), conn.buf.writer(), allocator, .varnum);
-                try conn.buf.append(0); // data length == 0
-                _ = try craft_io.encode(id, conn.buf.writer(), allocator, .varnum);
-                _ = try craft_io.encode(packet, conn.buf.writer(), allocator, encoding);
-                bytes_encoded += conn.buf.items.len;
-            }
-        },
-        .disabled => {
-            _ = try craft_io.encode(@as(i32, @intCast(payload_and_id_length)), conn.buf.writer(), allocator, .varnum);
+            // write the packet we actually intend to send
+            const actual_size: usize = (try craft_io.length(data_length, allocator, .varnum)) + comp.buf.items.len;
+            const conn_buf_writer = conn.buf.writer();
+            bytes_encoded += try craft_io.encode(@as(i32, @intCast(actual_size)), conn_buf_writer, allocator, .varnum);
+            bytes_encoded += try craft_io.encode(data_length, conn_buf_writer, allocator, .varnum);
+            try conn.buf.appendSlice(comp.buf.items);
+        } else {
+            _ = try craft_io.encode(@as(i32, @intCast(payload_and_id_length + 1)), conn.buf.writer(), allocator, .varnum);
+            try conn.buf.append(0); // data length == 0
             _ = try craft_io.encode(id, conn.buf.writer(), allocator, .varnum);
             _ = try craft_io.encode(packet, conn.buf.writer(), allocator, encoding);
             bytes_encoded += conn.buf.items.len;
-        },
+        }
+    } else {
+        _ = try craft_io.encode(@as(i32, @intCast(payload_and_id_length)), conn.buf.writer(), allocator, .varnum);
+        _ = try craft_io.encode(id, conn.buf.writer(), allocator, .varnum);
+        _ = try craft_io.encode(packet, conn.buf.writer(), allocator, encoding);
+        bytes_encoded += conn.buf.items.len;
     }
 
     const bytes_to_send = conn.buf.items;
+    if (conn.encryption) |*encryption| {
+        encryption.tx.encrypt(bytes_to_send);
+    }
+
     const n_bytes_written = bytes_to_send.len;
     try conn.socket.writeAll(bytes_to_send);
     log.debug(
@@ -252,25 +260,34 @@ pub fn setCompressionThreshold(conn: *Conn, threshold: usize) !void {
         return error.CompressionAlreadyEnabled;
     }
 
-    conn.compression = .{ .enabled = .{
+    conn.compression = .{
         .buf = std.ArrayList(u8).init(conn.allocator),
         .threshold = threshold,
-    } };
+    };
     log.debug("{*} compression enabled, threshold = {n}", .{ conn, threshold });
 }
 
 pub fn isCompressionEnabled(conn: *const Conn) bool {
-    return switch (conn.compression) {
-        .disabled => false,
-        .enabled => true,
-    };
+    return conn.compression != null;
+}
+
+pub fn enableEncryption(conn: *Conn, key: [cfb8.BYTES_SIZE]u8, iv: [cfb8.BYTES_SIZE]u8) !void {
+    if (conn.isEncryptionEnabled()) {
+        return error.EncryptionAlreadyEnabled;
+    }
+
+    conn.encryption = .{ .rx = .init(key, iv), .tx = .init(key, iv) };
+    log.debug("{*} encryption enabled", .{conn});
+}
+
+pub fn isEncryptionEnabled(conn: *Conn) bool {
+    return conn.encryption != null;
 }
 
 fn clearBuffers(conn: *Conn) void {
     conn.buf.clearRetainingCapacity();
-    switch (conn.compression) {
-        .enabled => |*e| e.buf.clearRetainingCapacity(),
-        .disabled => {},
+    if (conn.compression) |*e| {
+        e.buf.clearRetainingCapacity();
     }
 }
 
---
+++ src/cfb8.zig
@@ -0,0 +1,127 @@
+const std = @import("std");
+
+pub const BYTES_SIZE = 16;
+pub const Key = [BYTES_SIZE]u8;
+
+pub const Cipher = struct {
+    const Aes128Encrypt = std.crypto.core.aes.AesEncryptCtx(std.crypto.core.aes.Aes128);
+
+    iv: Key,
+    tmp: Key = .{0} ** BYTES_SIZE,
+    aes: Aes128Encrypt,
+
+    pub fn init(key: Key, iv: Key) Cipher {
+        return .{
+            .iv = iv,
+            .aes = Aes128Encrypt.init(key),
+        };
+    }
+
+    pub fn encrypt(cipher: *Cipher, data: []u8) void {
+        return applyStream(cipher.encryptStream(data), data);
+    }
+
+    pub fn decrypt(cipher: *Cipher, data: []u8) void {
+        return applyStream(cipher.decryptStream(data), data);
+    }
+
+    pub fn reader(cipher: *Cipher, upstream: anytype) Reader(@TypeOf(upstream)) {
+        return .{ .context = .{ .cipher = cipher, .upstream = upstream } };
+    }
+
+    pub fn Reader(comptime Upstream: type) type {
+        const Context = struct {
+            cipher: *Cipher,
+            upstream: Upstream,
+
+            fn read(context: @This(), buffer: []u8) Upstream.Error!usize {
+                const bytes_read = try context.upstream.read(buffer);
+                context.cipher.decrypt(buffer[0..bytes_read]);
+                return bytes_read;
+            }
+        };
+
+        return std.io.Reader(Context, Upstream.Error, Context.read);
+    }
+
+    const EncryptStream = Stream(struct {
+        pub fn updateIv(target: *u8, src_byte: u8, crypt_byte: u8) void {
+            _ = crypt_byte;
+            target.* = src_byte;
+        }
+    });
+
+    fn encryptStream(cipher: *Cipher, src: []const u8) EncryptStream {
+        return .{ .cipher = cipher, .src = src };
+    }
+
+    const DecryptStream = Stream(struct {
+        pub fn updateIv(target: *u8, src_byte: u8, crypt_byte: u8) void {
+            _ = src_byte;
+            target.* = crypt_byte;
+        }
+    });
+
+    fn decryptStream(cipher: *Cipher, src: []const u8) DecryptStream {
+        return .{ .cipher = cipher, .src = src };
+    }
+};
+
+fn Stream(comptime CipherOps: type) type {
+    return struct {
+        cipher: *Cipher,
+        src: []const u8,
+
+        pub fn next(stream: *@This()) ?u8 {
+            if (stream.src.len == 0) {
+                return null;
+            }
+
+            const src_byte = stream.src[0];
+            var val = src_byte;
+            @memcpy(&stream.cipher.tmp, &stream.cipher.iv);
+            stream.cipher.aes.encrypt(&stream.cipher.iv, &stream.cipher.iv);
+            val = val ^ stream.cipher.iv[0];
+            @memcpy(stream.cipher.iv[0 .. BYTES_SIZE - 1], stream.cipher.tmp[1..]);
+            CipherOps.updateIv(&stream.cipher.iv[BYTES_SIZE - 1], src_byte, val);
+            stream.src = stream.src[1..];
+            return val;
+        }
+    };
+}
+
+fn applyStream(s: anytype, data: []u8) void {
+    var stream = s;
+    var byte_idx: usize = 0;
+    while (stream.next()) |b| {
+        std.debug.assert(byte_idx < data.len);
+        data[byte_idx] = b;
+        byte_idx += 1;
+    }
+    std.debug.assert(data.len == byte_idx);
+}
+
+test "two side communication" {
+    var key: Key = undefined;
+    std.crypto.random.bytes(&key);
+
+    var rx: Cipher = .init(key, key);
+    var tx: Cipher = .init(key, key);
+
+    const msg = "hello world";
+    var msg_encrypted: [msg.len]u8 = undefined;
+    @memcpy(&msg_encrypted, msg);
+
+    rx.encrypt(&msg_encrypted);
+
+    var encrypted_stream = std.io.fixedBufferStream(&msg_encrypted);
+    const decrypted_reader = tx.reader(encrypted_stream.reader());
+
+    var decrypted_msg = std.ArrayList(u8).init(std.testing.allocator);
+    defer decrypted_msg.deinit();
+
+    try decrypted_reader.readAllArrayList(&decrypted_msg, 128);
+
+    try std.testing.expectEqual(msg.len, decrypted_msg.items.len);
+    try std.testing.expectEqualStrings(msg, decrypted_msg.items);
+}
